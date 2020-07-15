e965a873121f9d619a482d51f29b33d2add048e4
Hyeong gon Yoo <hyeyoo@c1r4s3.42seoul.kr>
2020-07-15 19:36:08 +0900

    error check when get_env_value failed

--- builtin/pwd.c
+++ builtin/pwd.c
@@ -6,7 +6,7 @@
 /*   By: hyeyoo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/11 15:45:34 by hyeyoo            #+#    #+#             */
-/*   Updated: 2020/07/14 16:04:11 by hyeyoo           ###   ########.fr       */
+/*   Updated: 2020/07/15 19:34:39 by hyeyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,52 +20,21 @@
 #include "error.h"
 
 #define TOO_MANY_ARGUMENTS		"too many arguments"
-#define BAD_OPTION				"bad option"
 #define EXE_NAME				"pwd"
 #define PWD						"PWD"
-
-int		option(char *argument, int *is_physical, char *param)
-{
-	param[0] = '-';
-	param[2] = '\0';
-	argument++;
-	while (*argument != '\0')
-	{
-		param[1] = *argument;
-		if (*argument == 'L')
-			*is_physical = (*is_physical) ? TRUE : FALSE;
-		else if (*argument == 'P')
-			*is_physical = TRUE;
-		else
-			return (FALSE);
-		argument++;
-	}
-	return (TRUE);
-}
+#define ENV_ERROR				"environment variables error"
 
 int		pwd(int argc, char **argv, char **environ)
 {
 	int		i;
 	int		is_physical;
+	char	*logical;
 	char	buf[PATH_MAX + 1];
-	char	param[3];
 
-	is_physical = FALSE;
-	i = 1;
-	while (i < argc)
-	{
-		if (*(argv[i]) != '-')
-			return (error_msg(EXE_NAME, TOO_MANY_ARGUMENTS));
-		else if (option(argv[i], &is_physical, param) == FALSE)
-			return (error_msg_param(EXE_NAME, BAD_OPTION, param));
-		i++;
-	}
-	if (is_physical)
-	{
-		getcwd(buf, PATH_MAX);
-		ft_putstr_endl(buf);
-	}
-	else
-		ft_putstr_endl(get_env_value(environ, PWD));
+	if (argc != 1)
+		return (error_msg(EXE_NAME, TOO_MANY_ARGUMENTS));
+	if ((logical = get_env_value(environ, PWD)) == NULL)
+		return (error_msg(EXE_NAME, ENV_ERROR));
+	ft_putstr_endl(logical);
 	return (EXIT_SUCCESS);
 }
