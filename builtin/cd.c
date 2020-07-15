37d0d5860d67553c6b39a77a0449ad85b05ed81d
Hyeong gon Yoo <hyeyoo@c1r4s3.42seoul.kr>
2020-07-15 20:35:25 +0900

    add cd ~ && absolute path bug fix

--- builtin/cd.c
+++ builtin/cd.c
@@ -6,7 +6,7 @@
 /*   By: hyeyoo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/11 15:26:13 by hyeyoo            #+#    #+#             */
-/*   Updated: 2020/07/15 19:35:44 by hyeyoo           ###   ########.fr       */
+/*   Updated: 2020/07/15 20:34:49 by hyeyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 #include "constant.h"
 
 #define PWD							"PWD"
@@ -41,39 +42,51 @@ int		update_pwd(char *pwd, char **environ)
 	return (-1);
 }
 
-int		get_previous(char *buf, char **environ)
+int		get_from_env(char *buf, char *key, char **environ)
 {
-	char	*oldpwd;
+	char	*value;
 
-	oldpwd = get_env_value(environ, OLDPWD);
-	if (oldpwd == NULL)
+	value = get_env_value(environ, key);
+	if (value == NULL)
 		return (error_msg(EXE_NAME, ENV_ERROR));
-	ft_strlcpy(buf, oldpwd, PATH_MAX);
+	ft_strlcpy(buf, value, PATH_MAX);
 	return (-1);
+}
+
+void	cd_sub(char *buf, char *path)
+{
+	struct stat		statbuf;
+	char			*slash;
+
+	if (lstat(path, &statbuf) == 0) {
+		ft_strlcpy(buf, path, PATH_MAX);
+	}
+	else
+	{
+		slash = (path[ft_strlen(path) - 1] == '/') ? "" : "/";
+		ft_strlcat(buf, slash, PATH_MAX);
+		ft_strlcat(buf, path, PATH_MAX);
+	}
 }
 
 int		cd(int argc, char **argv, char **environ)
 {
 	char	buf[PATH_MAX];
 	int		ret;
-	char	*slash;
 
 	ft_memset(buf, 0, PATH_MAX);
 	if (argc != 2)
 		return ((argc == 1) ? 0 : error_msg(EXE_NAME, TOO_MANY_ARGUMENTS));
 	else if (getcwd(buf, PATH_MAX) == NULL)
 		return (error_msg(EXE_NAME, GETCWD_FAILED));
-	else if (ft_strcmp(argv[1], "-") == 0)
+	else if (ft_strcmp(argv[1], "-") == 0 || ft_strcmp(argv[1], "~") == 0)
 	{
-		if ((ret = get_previous(buf, environ)) >= 0)
+		if ((ret = get_from_env(buf, *(argv[1]) == '-' ?
+						OLDPWD : HOME, environ)) >= 0)
 			return (ret);
 	}
 	else
-	{
-		slash = (argv[1][ft_strlen(argv[1]) - 1] == '/') ? "" : "/";
-		ft_strlcat(buf, slash, PATH_MAX);
-		ft_strlcat(buf, argv[1], PATH_MAX);
-	}
+		cd_sub(buf, argv[1]);
 	if (chdir(buf) == -1 || getcwd(buf, PATH_MAX) == NULL)
 		return (error_msg_param(EXE_NAME, strerror(errno), argv[1]));
 	if ((ret = update_pwd(buf, environ)) >= 0)
