3e36491b39c948a6fb377e7bc208a9a6b3d8f1fe
JIND <jinwkim@student.42seoul.kr>
2020-07-21 13:45:19 +0900

    add dup test code

--- srcs/execute_commands.c
+++ srcs/execute_commands.c
@@ -6,7 +6,7 @@
 /*   By: jinwkim <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/19 16:30:18 by jinwkim           #+#    #+#             */
-/*   Updated: 2020/07/20 14:21:27 by jinwkim          ###   ########.fr       */
+/*   Updated: 2020/07/21 13:43:55 by jinwkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@ int		execute_pipe(int idx, int *fd, char **argv, char ***env)
 	int		status;
 	char	**new_argv;
 
-	dup2(fd[1], 1);
-	dup2(fd[0], 0);
+	//dup2(fd[1], 1);
+	//dup2(fd[0], 0);
 	if (idx != 0)
 	{
 		child = fork();
@@ -61,6 +61,9 @@ int		execute_commands(char **argv, char ***env)
 		child = fork();
 		if (child == 0)
 		{
+			dup2(fd[1], 1);
+			close(fd[0]);
+			fd[0] = dup(0);
 			execute_pipe(len - 2, fd, argv, env);
 			exit(0);
 		}
@@ -70,9 +73,7 @@ int		execute_commands(char **argv, char ***env)
 	new_argv = ft_split(argv[len - 1], ' ');
 	execute_command(new_argv, env);
 	clean_arg(0, 0, &new_argv, 0);
-	write(fd[0], "\0", 1);
 	char c;
-
 	while (read(fd[0], &c, 1) > 0)
 	{
 		write(1, &c, 1);
